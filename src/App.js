4ac5ee138d2ace95bded71e2298d7df347831f1e
osama355 <muhammadosama3556@gmail.com>
2021-09-26 21:51:10 +0500

    update 1

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import Tracker from './component/tracker';
+import {Tracker} from './component/tracker'
+function App() {
+  return (
+    <div className="App">
+      <Tracker/>
+    </div>
+  );
+}
+
+export default App;
